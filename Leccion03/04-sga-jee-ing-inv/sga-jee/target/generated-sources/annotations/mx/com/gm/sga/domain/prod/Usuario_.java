6596ddd6e83ef0b2fa95b855df981c93363ec92d
diegoogle <48544269+diegoogle@users.noreply.github.com>
2019-09-10 11:41:03 -0500

    Add files via upload

---
+++ Leccion03/04-sga-jee-ing-inv/sga-jee/target/generated-sources/annotations/mx/com/gm/sga/domain/prod/Usuario_.java
@@ -0,0 +1,18 @@
+package mx.com.gm.sga.domain.prod;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.com.gm.sga.domain.Persona;
+import mx.com.gm.sga.domain.Usuario;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-30T20:53:28")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> password;
+    public static volatile SingularAttribute<Usuario, Persona> persona;
+    public static volatile SingularAttribute<Usuario, Integer> idUsuario;
+    public static volatile SingularAttribute<Usuario, String> username;
+
+}
