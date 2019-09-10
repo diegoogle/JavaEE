36ac7c51058956f1bb33f6d7c8737f5a4d2fb691
diegoogle <48544269+diegoogle@users.noreply.github.com>
2019-09-10 11:39:58 -0500

    Add files via upload

---
+++ Leccion03/03-sga-jee-jpa-console/sga-jee/target/generated-sources/annotations/mx/com/gm/sga/domain/dev/Persona_.java
@@ -0,0 +1,19 @@
+package mx.com.gm.sga.domain.dev;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.com.gm.sga.domain.Persona;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-30T17:24:57")
+@StaticMetamodel(Persona.class)
+public class Persona_ { 
+
+    public static volatile SingularAttribute<Persona, String> apeMaterno;
+    public static volatile SingularAttribute<Persona, String> telefono;
+    public static volatile SingularAttribute<Persona, String> nombre;
+    public static volatile SingularAttribute<Persona, String> apePaterno;
+    public static volatile SingularAttribute<Persona, Integer> idPersona;
+    public static volatile SingularAttribute<Persona, String> email;
+
+}
